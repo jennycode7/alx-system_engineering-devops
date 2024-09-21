be3fbc551299737130e98f4a4a360e6ed05dafa0
jennycode7 <jennessecosmas7@gmail.com>
2024-09-20 21:08:48 -0700

    installs flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',        # Specifies the version of Flask
   provider => 'pip3',         # Specifies that pip3 should be used
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.0',    # Downgrade to Werkzeug 2.1.0
+  provider => 'pip3',
+}
