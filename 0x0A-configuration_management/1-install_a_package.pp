5d8cd241d86dd5dfc34c607aea9d91a0843a0b4f
jennycode7 <jennessecosmas7@gmail.com>
2024-09-20 21:48:35 -0700

    for flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #installs flask
-package { 'flask':
-  ensure   => '2.1.0',        # Specifies the version of Flask
-  provider => 'pip3',         # Specifies that pip3 should be used
+exec { 'install_flask':
+  command => 'pip3 install flask==2.1.0',  # Command to install Flask version 2.1.0
+  path    => ['/usr/bin', '/bin'],         # Ensure correct path for pip3
+  unless  => 'pip3 show flask | grep "Version: 2.1.0"',  # Prevents re-installation if version 2.1.0 is already installed
 }
